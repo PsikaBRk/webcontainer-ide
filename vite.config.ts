ba0751b1da777d013502eb0d8bcb7db5bfb2ec93
Sunny-117 <zhiqiangfu6@gmail.com>
2025-12-03 21:43:20 +0800

    feat: deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/webcontainer-ide/',
   server: {
     headers: {
       'Cross-Origin-Embedder-Policy': 'require-corp',
